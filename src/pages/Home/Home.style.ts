019e4fb3f106fc38c7d594b5acff78968ace8acf
Seungwoo, Joo <thisis.joos@gmail.com>
2024-05-22 13:14:21 +0900

    feat: 지도 타입, 줌 컨트롤 추가

--- src/pages/Home/Home.style.ts
+++ src/pages/Home/Home.style.ts
@@ -3,4 +3,6 @@ import styled from "styled-components";
 export const StyledMapContainer = styled.div`
     width: 100%;
     height: 100vh;
+    position:relative;
+    overflow:hidden;
 `;
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -15,8 +15,15 @@ export const Home = () => {
             level: 3, // 지도의 확대 레벨
             mapTypeId: window.kakao.maps.MapTypeId.ROADMAP, // 지도종류
         }
-
         let map = new window.kakao.maps.Map(mapContainer, mapOption);   // 지도를 생성
+
+        // 일반 지도와 스카이뷰로 지도 타입을 전환할 수 있는 지도타입 컨트롤을 생성합니다
+        let mapTypeControl = new window.kakao.maps.MapTypeControl();
+        map.addControl(mapTypeControl, window.kakao.maps.ControlPosition.TOPRIGHT); // 지도 타입 컨트롤을 지도에 표시합니다
+
+        // 지도 확대 축소를 제어할 수 있는  줌 컨트롤을 생성합니다
+        let zoomControl = new window.kakao.maps.ZoomControl();
+        map.addControl(zoomControl, window.kakao.maps.ControlPosition.RIGHT);
 
         //-->  기본 맵 container, options, map 설정
 
