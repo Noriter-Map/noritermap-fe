c8b13d4960ff7ae9aff39c3b7b4c8526c064bfe1
seocylucky <chaeyeon2285@naver.com>
2024-05-27 03:42:33 +0900

    feat: useTransformQuery 훅 생성

---
+++ src/hooks/useTransformQuery.ts
@@ -0,0 +1,65 @@
+import { OptionsState } from "../recoil/OptionState";
+import { SearchOptionRequest } from "../types/SearchOption.type";
+
+export const transformOptionsToQueryParams = (
+  optionsState: OptionsState,
+  keyword: string,
+  curLatitude: string,
+  curLongitude: string,
+  page: number,
+  size: number,
+  sort?: string[]
+): SearchOptionRequest => {
+  let idrodr = "";
+
+  if (optionsState["실내외"]) {
+    if (optionsState["실내외"]["실내"]) {
+      idrodr = "indoor";
+    }
+    if (optionsState["실내외"]["실외"]) {
+      if (idrodr) {
+        idrodr += ",outdoor";
+      } else {
+        idrodr = "outdoor";
+      }
+    }
+  }
+
+  const category: string[] = [];
+  const installationLocations = [
+    "주택단지",
+    "도시공원",
+    "어린이집",
+    "놀이제공영업소",
+    "식품접객업소",
+    "주상복합",
+    "아동복지시설",
+    "종교시설",
+  ];
+  installationLocations.forEach((location, index) => {
+    if (optionsState["설치장소"] && optionsState["설치장소"][location]) {
+      category.push(`C${index + 1}`);
+    }
+  });
+
+  const prvt_pblc: string[] = [];
+  if (optionsState["민공구분"]) {
+    if (optionsState["민공구분"]["민간"]) {
+      prvt_pblc.push("PRIVATE");
+    }
+    if (optionsState["민공구분"]["공공"]) {
+      prvt_pblc.push("PUBLIC");
+    }
+  }
+
+  return {
+    keyword,
+    idrodr: idrodr || undefined,
+    category: category.length > 0 ? category.join(",") : undefined,
+    prvt_pblc: prvt_pblc.length > 0 ? prvt_pblc.join(",") : undefined,
+    curLatitude,
+    curLongitude,
+    page,
+    size,
+  };
+};
