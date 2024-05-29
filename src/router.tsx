82c5103bddfac6c2039320ceefa3fe2e63e0f861
seocylucky <chaeyeon2285@naver.com>
2024-05-29 09:30:25 +0900

    feat: 새로고침 버그 해결 작업 및 react-toastify 작업

--- src/router.tsx
+++ src/router.tsx
@@ -1,11 +1,44 @@
-import { Route, Routes } from 'react-router-dom';
-import {Home} from "./pages/Home/Home";
+import React, { useEffect, useState } from "react";
+import { Route, Routes, useNavigate } from "react-router-dom";
+import { Home } from "./pages/Home/Home";
+import { ToastContainer } from "react-toastify";
 
 export function Router() {
-    return (
-        <Routes>
-            <Route path="/" element={<Home />} />
-            <Route path="/p/place/:facilityId" element={<Home />} />
-        </Routes>
-    )
+  const navigate = useNavigate();
+  const [isReloaded, setIsReloaded] = useState(false);
+
+  useEffect(() => {
+    const navigationEntries = performance.getEntriesByType(
+      "navigation"
+    ) as PerformanceNavigationTiming[];
+    if (
+      !isReloaded &&
+      navigationEntries.length > 0 &&
+      navigationEntries[0].type === "reload"
+    ) {
+      setIsReloaded(true);
+      navigate("/");
+    }
+  }, [isReloaded, navigate]);
+
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/search/:keyword" element={<Home />} />
+        <Route path="/p/place/:facilityId" element={<Home />} />
+      </Routes>
+      <ToastContainer
+        style={{ width: "448px" }}
+        position="top-left"
+        autoClose={1000}
+        draggable
+        hideProgressBar={false}
+        closeOnClick
+        rtl={false}
+        theme="light"
+        limit={1}
+      />
+    </>
+  );
 }
