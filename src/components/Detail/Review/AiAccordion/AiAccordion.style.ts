468e5a09cafbda9372f5b9403de662add18e1105
seocylucky <chaeyeon2285@naver.com>
2024-05-29 09:39:12 +0900

    feat: 아코디언 컴포넌트 작업

---
+++ src/components/Detail/Review/AiAccordion/AiAccordion.style.ts
@@ -0,0 +1,65 @@
+import styled from "styled-components";
+
+export const StyledAccordionContainer = styled.div`
+  margin: 0 auto;
+  width: 410px;
+  height: fit-content;
+  border-radius: 12px;
+  background: #efefef;
+  /* border: 1px solid ; */
+  margin-top: 24px;
+  padding-left: 1.12rem;
+  padding-top: 0.69rem;
+  padding-right: 1.25rem;
+  padding-bottom: 0.8rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 1.31rem;
+`;
+
+export const StyledAccordionHeaderContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  gap: 5.45rem;
+  cursor: pointer;
+`;
+
+export const StyledAccordionHeaderWrapper = styled.div`
+  width: 280px;
+  display: flex;
+  flex-direction: row;
+  gap: 130px;
+  margin-left: 170px;
+`;
+
+export const StyledAccordionHeader = styled.div`
+  color: #9d9d9d;
+  font-size: 16px;
+  font-weight: 800;
+`;
+
+interface ArrowProps {
+  isAccordionOpen: boolean;
+}
+
+export const StyledArrowIcon = styled.img<ArrowProps>`
+  width: 10px;
+  transform: ${({ isAccordionOpen }) =>
+    isAccordionOpen ? "rotate(270deg)" : "rotate(90deg)"};
+  transition: transform 0.3s ease;
+`;
+
+export const StyledAccordionContent = styled.div<{ $isOpen: boolean }>`
+  width: 90%;
+  color: #000000;
+  font-size: 14px;
+  font-weight: 400;
+  line-height: 1.3;
+  margin-left: 0.5rem;
+  word-break: keep-all;
+
+  display: ${({ $isOpen }) => ($isOpen ? "block" : "none")};
+`;
---
+++ src/components/Detail/Review/AiAccordion/AiAccordion.tsx
@@ -0,0 +1,48 @@
+import { useState } from "react";
+import {
+  StyledAccordionContainer,
+  StyledAccordionContent,
+  StyledAccordionHeader,
+  StyledAccordionHeaderContainer,
+  StyledAccordionHeaderWrapper,
+  StyledArrowIcon,
+} from "./AiAccordion.style";
+import ArrowIcon from "../../../../assets/Right_Arrow.png";
+import { FacilityInfoRight } from "../../../../types/SearchOption.type";
+
+interface AiAccodianProps {
+  isReviewData: FacilityInfoRight;
+}
+
+export const AiAccordion = ({ isReviewData }: AiAccodianProps) => {
+  const [isAccordionOpen, setIsAccordionOpen] = useState(false);
+
+  const handleOpenAccordion = () => {
+    setIsAccordionOpen(!isAccordionOpen);
+  };
+  return (
+    <>
+      <StyledAccordionContainer>
+        <StyledAccordionHeaderContainer onClick={handleOpenAccordion}>
+          <StyledAccordionHeaderWrapper>
+            <StyledAccordionHeader>AI 최근 후기 요약</StyledAccordionHeader>
+            {isAccordionOpen ? (
+              <StyledArrowIcon
+                src={ArrowIcon}
+                isAccordionOpen={isAccordionOpen}
+              />
+            ) : (
+              <StyledArrowIcon
+                src={ArrowIcon}
+                isAccordionOpen={isAccordionOpen}
+              />
+            )}
+          </StyledAccordionHeaderWrapper>
+        </StyledAccordionHeaderContainer>
+        <StyledAccordionContent $isOpen={isAccordionOpen}>
+          {isReviewData.data.aiSummary}
+        </StyledAccordionContent>
+      </StyledAccordionContainer>
+    </>
+  );
+};
